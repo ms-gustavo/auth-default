3ea5a1ed127cb91aa9ea8a4f4b57128e4129845f
Gustavo Maia <ms.gustavo@outlook.com>
2024-10-17 17:55:29 -0300

    criacao de pasta para backend

--- backend/src/utils/serverStringErrorsAndCodes.ts
+++ backend/src/utils/serverStringErrorsAndCodes.ts
@@ -1,5 +1,5 @@
 export const serverStringErrorsAndCodes = {
-  P2002: "Email already exists",
+  P2002: {message: "Email already exists", code: 400},
   P2025: "Email not found",
   P2026: "ConfirmId not found",
   P2027: "User not found",
