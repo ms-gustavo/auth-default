f15b56cca65fc9206437e6a6785b5f5f0d74a440
Gustavo Maia <ms.gustavo@outlook.com>
2024-10-21 07:55:58 -0300

    adicao de cors

--- backend/src/shared/server.ts
+++ backend/src/shared/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from "../routes/AuthRoutes";
 import providerRoutes from "../routes/ProviderRoutes";
 import sessionMiddleware from "../middlewares/session.middleware";
 import passport from "../services/Passport/passport";
 
 const app = express();
+app.use(cors());
 app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
