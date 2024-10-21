08905b0550c99d330fd623a7cce8b3a2adfae03f
Gustavo Maia <ms.gustavo@outlook.com>
2024-10-21 07:57:27 -0300

    adicao de interfaces

---
+++ frontend/src/interfaces/interfaces.ts
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+
+export interface AuthLayoutProps {
+  title: string;
+  children: ReactNode;
+  footer: ReactNode;
+}
+
+export interface LoginFormProps {
+  onSwitch: () => void;
+}
+
+export interface InputFieldProps {
+  label: string;
+  type: string;
+  id: string;
+  value: string;
+  required?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface AuthContextProps {
+  token: string | null;
+  login: (token: string) => void;
+  logout: () => void;
+}
